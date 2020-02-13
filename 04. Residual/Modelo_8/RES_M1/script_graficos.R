88542e657a0c0bebdae61c3fd765091c52945b37
kdcloop <dsparrag@unal.edu.co>
2020-02-13 00:49:24 -0500

    Actualización 20-02-13
    Cambio en el script de generación de gráficos.
    
    Se simplifican las funciones, se adiciona la función de gráfico generador de perfiles plasmáticos poblacional e individual para cada sujeto.

--- 04. Residual/Modelo_8/RES_M1/script_graficos.R
+++ 04. Residual/Modelo_8/RES_M1/script_graficos.R
@@ -26,26 +26,6 @@ require(rlang)
 # Selección de directorio principal
 setwd(file.path('F:','Documentos','(Proyecto)_Estudio_PKPD','CEFEPIME',
                 '04. Residual', 'Modelo_8', 'RES_M1'))
-##########################################################################-
-# Carga de ajustes individuales
-y_1_fits <- read_csv("ChartsData/IndividualFits/y_1_fits.txt")
-y_1_observations <- read_csv("ChartsData/IndividualFits/y_1_observations.txt")
-
-y_1_fits %>% 
-ggplot(mapping = aes(x = time, y = pop)) +
-  geom_line() +
-  facet_wrap(~ID, ncol = 4)
-
-
-y_1_observations %>% 
-  ggplot(mapping = aes(x = time, y = y_1)) +
-  geom_point() +
-  facet_wrap(~ID, ncol = 4)
-
-
-
-
-
 
 ##########################################################################-
 # Bondad de ajuste -------------------------------------------------
@@ -88,24 +68,54 @@ G_PRED_OBS_PRED <-
   coord_cartesian(xlim = c(0,90), ylim = c(0,90))
 
 ##########################################################################-
-# Bondad de ajuste OBS vs IPRED
-G_PRED_OBS_IPRED <-
-  y_1_obsVsPred %>% 
-  ggplot(mapping = aes(x = indivPredMean, y = y_1, group = ID)) +
-  geom_point(shape = 1) + 
-  xlab('IPRED') + ylab('OBS') +
-  geom_abline(slope = 1, intercept = 0, lty = 'dotted') + 
-  # Modelo LOESS
-  geom_line(y_1_visualGuides, 
-            mapping =  aes(x = indivPred_spline_abscissa, y = indivPred_spline), 
-            inherit.aes = F, colour = 'red4') +
-  geom_ribbon(y_1_visualGuides, 
-              mapping =  aes(x = indivPred_ci_abscissa, 
-                             ymin = indivPred_piLower,
-                             ymax = indivPred_piUpper), 
-              inherit.aes = F, fill = 'red1', alpha = 0.1) +
-  coord_cartesian(xlim = c(0,90), ylim = c(0,90))
+GOF_PRED <- function(x, y, xspline, yspline, xconfint, yconfint_lo, 
+                     yconfint_up, colourp, xlab, ylab) {
+  x = rlang::ensym(x)
+  y = rlang::ensym(y)
+  xspline = rlang::ensym(xspline)
+  yspline = rlang::ensym(yspline)
+  xconfint = rlang::ensym(xconfint)
+  yconfint_lo = rlang::ensym(yconfint_lo)
+  yconfint_up = rlang::ensym(yconfint_up)
   
+  y_1_obsVsPred %>% 	
+    ggplot(mapping = aes(x = !!x, y = !!y, group = ID)) +	
+    geom_point(shape = 1) + 	
+    xlab(xlab) + ylab(ylab) +	
+    geom_abline(slope = 1, intercept = 0, lty = 'dotted') + 	
+    geom_line(y_1_visualGuides, 	
+              mapping =  aes(x = !!xspline, y = !!yspline), 	
+              inherit.aes = F, colour = colourp) +	
+    geom_ribbon(y_1_visualGuides, 	
+                mapping =  aes(x = !!xconfint, 	
+                               ymin = !!yconfint_lo,	
+                               ymax = !!yconfint_up), 	
+                inherit.aes = F, fill = colourp, alpha = 0.1) +	
+    coord_cartesian(xlim = c(0,90), ylim = c(0,90))	%>% 
+    return(.)
+}
+
+G_PRED_OBS_PRED <- 
+  GOF_PRED(x = popPred, y = y_1, 
+         xspline = popPred_spline_abscissa, 
+         yspline = popPred_spline, 
+         xconfint = popPred_ci_abscissa, 
+         yconfint_lo = popPred_piLower, 
+         yconfint_up = popPred_piUpper, 
+         colourp = 'blue1', 
+         xlab = 'PRED', ylab = 'OBS')
+
+G_PRED_OBS_IPRED <- 
+  GOF_PRED(x = indivPredMean, y = y_1, 
+           xspline = indivPred_spline_abscissa, 
+           yspline = indivPred_spline, 
+           xconfint = indivPred_ci_abscissa, 
+           yconfint_lo = indivPred_piLower, 
+           yconfint_up = indivPred_piUpper, 
+           colourp = 'red1', 
+           xlab = 'IPRED', ylab = 'OBS')
+
+
 ##########################################################################-
 # Bondad de ajuste OBS vs IPRED
 G_PRED_OBS_PPRED <- 
@@ -187,68 +197,65 @@ y_1_spline <-
 ##########################################################################-
   # Gráficos de residuales vs Tiempo --------------------------------------
 ##########################################################################-
-
-G_RES_T_PWRES <-
-  y_1_residuals %>% 
-    ggplot(mapping = aes(x = time, y = pwRes)) +
-    geom_hline(yintercept = 0) +
-    geom_point(col = '#4682B4') +
-    geom_line(y_1_spline, 
-              mapping = aes(x = time_pwRes, y = time_pwRes_spline), 
-              col = '#EBA213', lty = 'solid', size = 1) +
-    geom_line(y_1_time_percentiles_pwRes, 
-              mapping = aes(x = time, y = empirical_median), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) + 
-    geom_line(y_1_time_percentiles_pwRes, 
-            mapping = aes(x = time, y = empirical_lower), 
-            col = '#EBA213', lty = 'dotted', size = 1.2) +
-    geom_line(y_1_time_percentiles_pwRes, 
-              mapping = aes(x = time, y = empirical_upper), 
-              col = '#EBA213', lty = 'dotted', size = 1.2) +
-    coord_cartesian(ylim = c(-2.5, 2.5)) +
-    xlab('TSFD') + ylab('PWRES')
-  
-G_RES_T_IWRES <-
-  y_1_residuals %>% 
-    ggplot(mapping = aes(x = time, y = iwRes_mean)) +
-    geom_hline(yintercept = 0) +
-    geom_line(y_1_spline, 
-              mapping = aes(x = time_pwRes, y = time_iwRes_spline), 
-              col = '#EBA213', lty = 'solid', size = 1) +
+RES_TSFD <- function(x, y, xspline, yspline, perc_data, xlab, ylab) {
+  x <- rlang::ensym(x)
+  y <- rlang::ensym(y)
+  xspline <- rlang::ensym(xspline)
+  yspline <- rlang::ensym(yspline)
+  stopifnot(is.data.frame(perc_data) == TRUE)
     
-    geom_line(y_1_time_percentiles_iwRes, 
-              mapping = aes(x = time, y = empirical_median), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) + 
-    geom_line(y_1_time_percentiles_iwRes, 
-              mapping = aes(x = time, y = empirical_lower), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) +
-    geom_line(y_1_time_percentiles_iwRes, 
-              mapping = aes(x = time, y = empirical_upper), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) +
-    geom_point(col = '#4682B4') +
-    coord_cartesian(ylim = c(-2.5, 2.5)) +
-    xlab('TSFD') + ylab('IWRES')
-  
-G_RES_T_NPDE <-
   y_1_residuals %>% 
-    ggplot(mapping = aes(x = time, y = npde)) +
+    ggplot(mapping = aes(x = !!x, y = !!y)) +
     geom_hline(yintercept = 0) +
-    geom_line(y_1_spline, 
-              mapping = aes(x = time_pwRes, y = time_npde_spline), 
-              col = '#EBA213', lty = 'solid', size = 1) +
-    geom_line(y_1_time_percentiles_npde, 
-              mapping = aes(x = time, y = empirical_median), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) + 
-    geom_line(y_1_time_percentiles_npde, 
-              mapping = aes(x = time, y = empirical_lower), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) +
-    geom_line(y_1_time_percentiles_npde, 
-              mapping = aes(x = time, y = empirical_upper), 
-              col = '#EBA213', lty = 'dashed', size = 1.2) +
     geom_point(col = '#4682B4') +
+    geom_line(y_1_spline, 
+              mapping = aes(x = !!xspline, y = !!yspline), 
+              col = '#EBA213', lty = 'solid', size = 1) +
+    geom_line(perc_data, 
+              mapping = aes(x = !!x, y = empirical_median), 
+              col = '#EBA213', lty = 'dashed', size = 1.2) + 
+    geom_line(perc_data, 
+              mapping = aes(x = !!x, y = empirical_lower), 
+              col = '#EBA213', lty = 'dashed', size = 1.2) +
+    geom_line(perc_data, 
+              mapping = aes(x = !!x, y = empirical_upper), 
+              col = '#EBA213', lty = 'dashed', size = 1.2) +
     coord_cartesian(ylim = c(-2.5, 2.5)) +
-    xlab('TSFD') + ylab('NPDE')
-  
+    xlab(xlab) + ylab(ylab) %>% return(.)
+}
+
+G_RES_T_PWRES <- RES_TSFD(
+  x = time,
+  y = pwRes,
+  xspline = time_pwRes,
+  yspline = time_pwRes_spline,
+  perc_data = y_1_time_percentiles_pwRes,
+  xlab = 'TSFD',
+  ylab = 'PWRES'
+)
+
+G_RES_T_IWRES <- RES_TSFD(
+  x = time,
+  y = iwRes_mean,
+  xspline = time_iwRes,
+  yspline = time_iwRes_spline,
+  perc_data = y_1_time_percentiles_iwRes,
+  xlab = 'TSFD',
+  ylab = 'IWRES'
+)
+
+G_RES_T_NPDE <- RES_TSFD(
+  x = time,
+  y = npde,
+  xspline = time_npde,
+  yspline = time_npde_spline,
+  perc_data = y_1_time_percentiles_npde,
+  xlab = 'TSFD',
+  ylab = 'NPDE'
+)
+
+##########################################################################-
+# Almacenar los archivos
 
 ggsave(filename = './FIGURAS/G_RES_T_PWRES.pdf', plot = G_RES_T_PWRES, 
        device = 'pdf', width = 5/1.5, height = 4/1.5, units = 'in')
@@ -277,7 +284,7 @@ ggsave(filename = './FIGURAS/G_RES_T_NPDE.pdf', plot = G_RES_T_NPDE,
 ##  de últimnas observaciones calculadas.
 ##:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-data <- read_delim("../1_DATA/Monolix_data.csv", delim = ';')
+data <- read_delim("Monolix_data.csv", delim = ';')
 
 last_dose <- data %>% 
   group_by(ID) %>% 
@@ -426,6 +433,32 @@ ggsave('./FIGURAS/G_RES_C_IWRES.pdf', G_RES_C_IWRES,
 ggsave('./FIGURAS/G_RES_C_NPDE.pdf', G_RES_C_NPDE, 
        device = 'pdf', width = 5/1.5, height = 4/1.5, units = 'in')  
 
+##########################################################################-
+# Gráficos individuales-----------------------------------------------------
+##########################################################################-
+# Carga de ajustes individuales
+y_1_fits <- read_csv("ChartsData/IndividualFits/y_1_fits.txt")
+y_1_observations <- read_csv("ChartsData/IndividualFits/y_1_observations.txt") %>% 
+  left_join(., last_dose, by = 'ID') %>% 
+  mutate(TAD = time - TIME)
+
+G_CP_TAD <-
+  y_1_fits %>%
+  left_join(., last_dose, by = 'ID') %>%
+  mutate(TAD = time - TIME) %>%
+  
+  ggplot(mapping = aes(x = TAD)) +
+  geom_line(aes(y = pop), lty = 'dashed') +
+  geom_line(aes(y = indivPredMean), lty = 'solid', col = 'green3') +
+  facet_wrap( ~ ID, ncol = 4, labeller = labeller(.cols = label_both)) +
+  geom_point(data = y_1_observations,
+             mapping = aes(x = TAD, y = y_1),
+             col = 'green4') +
+  xlim(0, NA) +
+  xlab('TAD') + ylab(expression(C[P]))
+  
+ggsave(filename = 'FIGURAS/G_CP_TAD.pdf', plot = G_CP_TAD,
+       device = 'pdf', width = 5, height = 6)
 
 ##########################################################################-
 # Cálculo manual de encogimiento eta y epsilon ----------------------------
@@ -433,8 +466,32 @@ ggsave('./FIGURAS/G_RES_C_NPDE.pdf', G_RES_C_NPDE,
 eta <- 
   read_csv("ChartsData/CorrelationBetweenRandomEffects/eta.txt")
 
-eta_shrink_V <- 1 - (var(eta$eta_V_mean) / (0.278838288339044 ^ 2))
-eta_shrink_Cl <- 1-(var(eta$eta_Cl_mean)/(0.299625514726524^2))
+pop_eta <-
+  read_csv("populationParameters.txt")
+
+pop_eta1 <- pop_eta %>% 
+  rownames_to_column %>% 
+  gather(var, value, -rowname) %>% 
+  spread(rowname, value) 
+
+colnames(pop_eta1) <- pop_eta1[1,]
+
+pop_eta1 <- pop_eta1[-1, ] %>%
+  filter(parameter == 'value') %>%
+  select_at(vars(matches("omega\\_"))) %>% 
+  mutate_all(~as.double(.))
+
+
+var_eta <- eta %>% 
+  summarise_at(., vars(matches("\\_mean")), .funs = list(var = ~ var(.))) %>%
+  rename_all(.funs = list(~ str_replace(., "\\_mean\\_var", ''))) %>%
+  rename_all(.funs = list(~ str_replace(., "eta\\_", ''))) %>%
+  rename_all(.funs = list(~ paste0(., '_pop'))) 
+  
+# Resultados de Eta
+eta_res <- 1 - (var_eta / (pop_eta1^ 2))
+
+# Resultados de IWRES
 epsilon_shrink <- 1-sd(y_1_residuals$iwRes_mean)
 
 
